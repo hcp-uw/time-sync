6fdb4fd99cb7578779af5bb5264dfe195e424e45
zchen-66 <andrewzitongchen@gmail.com>
2024-02-13 19:03:24 -0800

    did header stuff

---
+++ starter-frontend/src/Head.jsx
@@ -0,0 +1,11 @@
+function Head() {
+    return (
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet"></link>
+        </head>
+    );
+}
+
+export default Head
