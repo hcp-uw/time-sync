a4d8d2b4b858c9c002b8dafb6113e579af698b23
Victor Liu <vkliu@uw.edu>
2024-02-20 16:52:10 -0800

    Revamped all organization, created folders for images, components, and pages. Implemented router with pages so navbar is functional.

---
+++ starter-frontend/src/pages/Join.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import Head from '../components/Head';
+
+function Create() {
+    return (
+        <>
+            <Head />
+            <Header />
+            <h1>This is my Join Page</h1>
+            <h1>Hello, HCP. We are going to sync you in places you did not know before.</h1>
+        </>
+    )
+}
+
+export default Create
