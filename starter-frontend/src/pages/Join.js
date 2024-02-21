abf6911c3f39bf77a4ff58567e754ae39cfee8ef
Victor Liu <vkliu@uw.edu>
2024-02-20 19:58:35 -0800

    Added body component for background of first few pages. Will overlay components on top of it.

--- starter-frontend/src/pages/Join.js
+++ starter-frontend/src/pages/Join.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import Head from '../components/Head';
+import Body from '../components/Body';
 
 function Create() {
     return (
@@ -8,6 +9,7 @@ function Create() {
             <Header />
             <h1>This is my Join Page</h1>
             <h1>Hello, HCP. We are going to sync you in places you did not know before.</h1>
+            <Body />
         </>
     )
 }
