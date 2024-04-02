92b8baed9e0ec0594adaa6adaf46c1ec53502b64
Victor Liu <vkliu@uw.edu>
2024-04-02 16:50:27 -0700

    My goat.

---
+++ starter-frontend/src/pages/Sync.js
@@ -0,0 +1,13 @@
+import Head from '../components/Head';
+import SyncComponent from '../components/Sync';
+
+function Sync() {
+    return (
+        <>
+            <Head />
+            <SyncComponent />
+        </>
+    )
+}
+
+export default Sync
