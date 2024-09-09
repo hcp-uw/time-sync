157e5fca357e073d50dc45d83357dd0b8e12f5e1
Victor Liu <vkliu@uw.edu>
2024-09-08 21:24:08 -0700

    Fixed bug, sensative info all hidden

--- starter-frontend/src/config/firebase.js
+++ starter-frontend/src/config/firebase.js
@@ -1,13 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 import firebase from 'firebase/compat/app';
 import "firebase/compat/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env)
+
+// Firebase configuration loaded from environment variables in the .env file
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -16,16 +19,17 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
+// Initialize Firebase only once
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 } else {
-  app = getApps()[0]; // If already initialized, use the existing instance
+  firebase.app(); // if already initialized, use that one
 }
-const db = firebase.firestore();
+
+const db = firebase.firestore(); // Initialize Firestore
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
