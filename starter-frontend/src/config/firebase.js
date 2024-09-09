6a7372ff26b18803daacc50ed3412c6bdbf73b8c
Victor Liu <vkliu@uw.edu>
2024-09-08 20:32:47 -0700

    Hide sensative firebase config in hidden .env file

--- starter-frontend/src/config/firebase.js
+++ starter-frontend/src/config/firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { initializeApp, getApps } from "firebase/app";
 import firebase from 'firebase/compat/app';
 import "firebase/compat/firestore";
 
@@ -11,17 +9,22 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA9kKYERQlumTvH3pPFonAYBLEPX5SWxOw",
-  authDomain: "timesync-7171d.firebaseapp.com",
-  databaseURL: "https://timesync-7171d-default-rtdb.firebaseio.com",
-  projectId: "timesync-7171d",
-  storageBucket: "timesync-7171d.appspot.com",
-  messagingSenderId: "909312169478",
-  appId: "1:909312169478:web:271ce2a5e3f210ddd71587",
-  measurementId: "G-H1Q8Z22P9N"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0]; // If already initialized, use the existing instance
+}
 const db = firebase.firestore();
 
 // Initialize Firebase
